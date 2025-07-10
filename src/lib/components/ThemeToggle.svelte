2afd6382f239fd2d7ad05dbbb4962dd92e288631
AgungBahtiarr <agungbahtiar050@gmail.com>
2025-07-10 10:38:22 +0700

    Add theme toggle component and store

---
+++ src/lib/components/ThemeToggle.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { theme } from '../stores/themeStore';
+	import { Moon, Sun } from '@lucide/svelte';
+
+	function toggleTheme() {
+		theme.update((currentTheme) => (currentTheme === 'dark' ? 'laros' : 'dark'));
+	}
+</script>
+
+<label class="swap swap-rotate">
+	<input type="checkbox" on:change={toggleTheme} checked={$theme === 'dark'} />
+	<Sun class="swap-on h-6 w-6 fill-current" />
+	<Moon class="swap-off h-6 w-6 fill-current" />
+</label>
